47ee16061ef52ca6272c0a14537236cc2bae81dc
Samantha Randolph <samcollegeemail@gmail.com>
2026-07-30 15:27:05 -0700

    Added middleware to inject global data and made header course list dynamic.

--- server.js
+++ server.js
@@ -3,29 +3,53 @@ import express from "express";
 const PORT = 8000;
 
 const app = express();
-const year = new Date().getFullYear();
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.use(express.static('assets'));
 
+function injectGlobals(req, res, next) {
+  const year = new Date().getFullYear();
+  const courses = [
+    { name: "Computer Science", path: "coding" },
+    { name: "English", path: "english" },
+    { name: "Math", path: "math" },
+  ];
+
+  req.data = {
+    year,
+    courses,
+  };
+  next();
+}
+
+app.use(injectGlobals);
+
 app.get("/", (req, res) => {
   // TODO: add a globals middleware or something to add in year/menu data 
   // on every page automatically
   const data = {
+    ...req.data,
     title: "Samantha Randolph",
-    year,
   };
   res.render('index', data);
 });
 
 app.get("/about", (req, res) => {
   const data = {
+    ...req.data,
     title: "About - Samantha Randolph",
-    year,
   };
   res.render('about', data);
+});
+
+app.get("/courses", (req, res) => {
+  const data = {
+    ...req.data,
+    title: "Courses - Samantha Randolph",
+  };
+  res.render('courses', data);
 });
 
 app.listen(PORT, () => {
